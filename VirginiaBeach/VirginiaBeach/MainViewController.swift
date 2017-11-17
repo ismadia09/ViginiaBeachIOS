ab227101ce3f1dd0a5e414d3adec0a46dda19b96
Diallo <ismadia09@gmail.com>
2017-11-17 10:34:26 +0100

    Remove story board

---
+++ VirginiaBeach/VirginiaBeach/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  VirginiaBeach
+//
+//  Created by Isma Dia on 17/11/2017.
+//  Copyright © 2017 Isma Dia. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
